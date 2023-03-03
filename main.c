9a881042718b17dffe5a0d69bbb498b8035336a5
SerjeiMikailov <felipemaders8@gmail.com>
2023-03-03 00:20:04 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("-----------\n");
+    printf("- Welcome -\n");
+    printf("-----------\n");
+
+    int secretnumber = 30;
+
+    printf("O número é: %d", secretnumber);
+}
