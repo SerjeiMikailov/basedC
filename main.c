58936375a6e253f7d40d732df9f59d8b2405465d
SerjeiMikailov <felipemaders8@gmail.com>
2023-03-03 20:01:12 -0300

    g

--- main.c
+++ main.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
 
-int main() {
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
     printf("-----------\n");
     printf("- Welcome -\n");
     printf("-----------\n");
 
     int secretnumber = 30;
 
-    printf("O número é: %d", secretnumber);
+    int guess;
+
+    printf("Qual seu chute? ");
+    scanf("%d", &guess);
+    printf("Seu chute foi %d\n", guess);
+
+    if (guess == secretnumber)
+    {
+        printf("Você acertou\n");
+    }
+    else
+    {
+        if(guess > secretnumber) {
+            printf("O número que você chutou está acima do número certo\n");
+        } else {
+            printf("O número que você chutou está abaixo do correto\n");
+        }
+    }
 }
